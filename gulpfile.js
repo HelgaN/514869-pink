6b9f66e157c1531f1331f4060264dfd48def2717
HelgaN <ola14@tut.by>
2018-05-06 00:49:18 +0300

    Автоматизация сборки

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var del = require("del");
@@ -12,8 +14,10 @@ var svgstore = require("gulp-svgstore");
 var minify = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
+var server = require("browser-sync").create();
+var run = require("run-sequence");
 
-gulp.task("style", function() {
+/*gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
@@ -25,6 +29,27 @@ gulp.task("style", function() {
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});*/
+gulp.task("style", function() {
+  gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("build"));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -63,7 +88,7 @@ gulp.task("sprite", function() {
   }))
 
   .pipe(rename("sprite.svg"))
-  .pipe(gulp.dest("source/img"));
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("images", function() {
@@ -80,4 +105,15 @@ gulp.task("webp", function() {
   return gulp.src("source/img/**/*.{png, jpg}")
   .pipe(webp({quality: 90}))
   .pipe(gulp.dest("source/img/1"));
+});
+
+gulp.task("build", function(done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "sprite",
+    "html",
+    done
+  );
 });
